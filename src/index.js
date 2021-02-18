d9bc5b9d46c30147364658aa4ad67d811fd48ed6
Shimiiy <bangchaq2002@gmail.com>
2021-02-18 14:06:02 +0900

    this is the code I wanted to compare with

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 function Square(props) {
   return (
-    <button classname ="square" onClick={props.onClick}>
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -29,11 +29,11 @@ class Board extends React.Component {
       xIsNext: !this.state.xIsNext,
     });
   }
-  
+
   renderSquare(i) {
     return (
-      <Square 
-        value = {this.state.squares[i]} 
+      <Square
+        value={this.state.squares[i]}
         onClick={() => this.handleClick(i)}
       />
     );
@@ -107,7 +107,7 @@ function calculateWinner(squares) {
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] && squares [a] === squares[b] && squares[a] === squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
